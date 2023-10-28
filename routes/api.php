257cad6be34f989a0d7efe7cb9b85001e48689aa
andrew21-mch <nfonandrew73@gmail.com>
2023-10-29 00:39:13 +0100

    setup routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CampaignController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
@@ -8,6 +9,11 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::put('/user', [AuthController::class, 'updateUser']);
     Route::put('/user/password', [AuthController::class, 'changePassword']);
+
+    // Campaign routes
+    Route::post('/campaigns', [CampaignController::class, 'createCampaign']);
+    Route::put('/campaigns/{campaign_id}', [CampaignController::class, 'updateCampaign']);
+    Route::delete('/campaigns/{campaign_id}', [CampaignController::class, 'deleteCampaign']);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -17,4 +23,12 @@ Route::post('password/reset/{token}', [AuthController::class, 'resetPassword'])-
 Route::group(['prefix' => 'email'], function () {
     Route::get('/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
     Route::post('/resend', [AuthController::class, 'resendVerificationEmail'])->name('verification.resend');
+});
+
+// Campaign routes
+Route::group(['prefix' => 'campaigns'], function () {
+    Route::get('/', [CampaignController::class, 'getAllCampaigns']);
+    Route::get('/{campaign_id}', [CampaignController::class, 'getCampaign']);
+    Route::post('/{campaign_id}/donations', [CampaignController::class, 'makeDonation']);
+    Route::get('/search', [CampaignController::class, 'searchCampaign']);
 });
