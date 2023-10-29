8f872f58a664d0111546e78336436ddbfddb01db
andrew21-mch <nfonandrew73@gmail.com>
2023-10-29 04:07:07 +0100

    update: finish basic test for campaigns

--- tests/Feature/API/CampaignControllerTest.php
+++ tests/Feature/API/CampaignControllerTest.php
@@ -85,32 +85,98 @@ class CampaignControllerTest extends TestCase
 
         $this->assertDatabaseHas('campaigns', ['id' => $campaign->id]);
     }
-    
+
     public function test_update_campaign()
-{
-    $token = $this->authenticate();
+    {
+        $token = $this->authenticate();
 
-    // Create a campaign record in the database
-    $campaign = Campaign::factory()->create();
+        // Create a campaign record in the database
+        $campaign = Campaign::factory()->create();
 
-    $updatedData = [
-        'title' => 'Updated Campaign',
-        'description' => 'This is an updated campaign.',
-        'goal_amount' => 2000,
-    ];
+        $updatedData = [
+            'title' => 'Updated Campaign',
+            'description' => 'This is an updated campaign.',
+            'goal_amount' => 2000,
+        ];
 
-    $response = $this->put('/api/campaigns/' . $campaign->id, $updatedData, $this->getHeaders($token));
+        $response = $this->put('/api/campaigns/' . $campaign->id, $updatedData, $this->getHeaders($token));
 
-    $response->assertOk()
-        ->assertJsonStructure(['data' => ['campaign']])
-        ->assertJson([
-            'data' => [
-                'campaign' => $updatedData
-            ]
+        $response->assertOk()
+            ->assertJsonStructure(['data' => ['campaign']])
+            ->assertJson([
+                'data' => [
+                    'campaign' => $updatedData
+                ]
+            ]);
+
+        $this->assertDatabaseHas('campaigns', $updatedData);
+    }
+
+    public function test_delete_campaign()
+    {
+        $token = $this->authenticate();
+
+        // Create a campaign record in the database
+        $campaign = Campaign::factory()->create();
+
+        $response = $this->delete('/api/campaigns/' . $campaign->id, [], $this->getHeaders($token));
+
+        $response->assertOk();
+
+        $this->assertDatabaseMissing('campaigns', ['id' => $campaign->id]);
+    }
+
+    public function test_make_donation()
+    {
+        $token = $this->authenticate();
+
+        // Create a campaign record in the database
+        $campaign = Campaign::factory()->create();
+
+        $initialAmount = $campaign->current_amount;
+
+        $donationData = [
+            'amount' => 100,
+        ];
+
+        $response = $this->post('/api/campaigns/' . $campaign->id . '/donate', $donationData, $this->getHeaders($token));
+
+        $response->assertCreated()
+            ->assertJsonStructure(['data' => ['donation']])
+            ->assertJson([
+                'data' => [
+                    'donation' => [
+                        'amount' => $donationData['amount'],
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseHas('donations', [
+            'campaign_id' => $campaign->id,
+            'amount' => $donationData['amount'],
         ]);
 
-    $this->assertDatabaseHas('campaigns', $updatedData);
-}
+        $campaign = $campaign->fresh(); // Refresh the campaign model from the database
+        $newAmount = $initialAmount + $donationData['amount'];
+        $this->assertEquals($newAmount, $campaign->current_amount);
+    }
+
+    public function test_search_campaign()
+    {
+        $token = $this->authenticate();
+
+        // Create three campaign records in the database
+        $campaigns = Campaign::factory()->count(3)->create();
+
+        $searchQuery = 'nothing';
+
+        $url = '/api/campaigns/search-campaigns/get?query=' . urlencode($searchQuery);
+
+        $response = $this->get($url, $this->getHeaders($token));
+
+        $response->assertOk()
+            ->assertJsonStructure(['data' => ['campaigns']]);
+    }
     private function getHeaders($token = null)
     {
         $headers = [];
