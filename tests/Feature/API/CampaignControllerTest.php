2ae5c2a50c729011cc9abf520f0280d95c2c7d2f
andrew21-mch <nfonandrew73@gmail.com>
2023-10-29 03:46:23 +0100

    update: test get single campaign by id

--- tests/Feature/API/CampaignControllerTest.php
+++ tests/Feature/API/CampaignControllerTest.php
@@ -55,10 +55,36 @@ class CampaignControllerTest extends TestCase
         $response = $this->get('/api/campaigns');
 
         $response->assertOk()
-        ->assertJsonStructure(['data' => ['campaigns']])
-        ->assertJsonCount(3, 'data.campaigns');
+            ->assertJsonStructure(['data' => ['campaigns']])
+            ->assertJsonCount(3, 'data.campaigns');
         $campaignIds = $campaigns->pluck('id')->all();
         $this->assertDatabaseHas('campaigns', ['id' => $campaignIds]);
+    }
+
+    public function test_get_campaign()
+    {
+        $token = $this->authenticate();
+
+        // Create a campaign record in the database
+        $campaign = Campaign::factory()->create();
+
+        $response = $this->get('/api/campaigns/' . $campaign->id, $this->getHeaders($token));
+
+        $response->assertOk()
+            ->assertJsonStructure(['data' => ['campaign']])
+            ->assertJson([
+                'data' => [
+                    'campaign' => [
+                        'id' => $campaign->id,
+                        'title' => $campaign->title,
+                        'description' => $campaign->description,
+                        'goal_amount' => $campaign->goal_amount,
+                        // Add any additional attributes you want to assert
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseHas('campaigns', ['id' => $campaign->id]);
     }
     private function getHeaders($token = null)
     {
