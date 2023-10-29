cc27db4222bcceed650cf2fcce9887493ea74b07
andrew21-mch <nfonandrew73@gmail.com>
2023-10-29 03:10:44 +0100

    update: round up with AuthController test

--- tests/Feature/API/AuthControllerTest.php
+++ tests/Feature/API/AuthControllerTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use DB;
 use Hash;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
@@ -153,5 +156,97 @@ class AuthControllerTest extends TestCase
         // Assert that the user's name and email have been updated correctly
         $this->assertEquals($newName, $user->fresh()->name);
         $this->assertEquals($newEmail, $user->fresh()->email);
+    }
+
+    public function test_change_password()
+    {
+        // Create a user
+        $user = User::factory()->create([
+            'password' => bcrypt('old_password'),
+        ]);
+
+        // Simulate the authenticated user
+        $this->actingAs($user);
+
+        // Send a request to change the password
+        $response = $this->put('api/user/password', [
+            'current_password' => 'old_password',
+            'new_password' => 'new_password',
+            'new_password_confirmation' => 'new_password',
+        ]);
+
+        // Assert the response
+        $response->assertStatus(Response::HTTP_OK);
+        $this->assertTrue(Hash::check('new_password', $user->fresh()->password));
+    }
+
+    public function test_send_reset_link_email()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Send a request to send the password reset link
+        $response = $this->post('api/password/email', [
+            'email' => $user->email,
+        ]);
+
+        // Assert the response
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonStructure(['data' => ['token']]);
+        $this->assertDatabaseHas('password_reset_tokens', ['email' => $user->email]);
+    }
+
+    public function test_reset_password()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Generate a password reset token
+        $token = Str::random(8);
+        DB::table('password_reset_tokens')->insert([
+            'email' => $user->email,
+            'token' => Hash::make($token),
+            'created_at' => Carbon::now(),
+        ]);
+
+        // Send a request to reset the password
+        $response = $this->post('api/password/reset/' . $token, [
+            'email' => $user->email,
+            'password' => 'new_password',
+            'password_confirmation' => 'new_password',
+        ]);
+
+        // Assert the response
+        $response->assertStatus(Response::HTTP_OK);
+        $this->assertTrue(Hash::check('new_password', $user->fresh()->password));
+        $this->assertDatabaseMissing('password_reset_tokens', ['email' => $user->email]);
+    }
+
+    public function test_verify_email()
+    {
+        // Create a user
+        $user = User::factory()->create(['email_verified_at' => null]);
+
+        // Send a request to verify the email
+        $response = $this->get('api/email/verify/' . $user->id . '/' . sha1($user->getEmailForVerification()));
+
+        // Assert the response
+        $response->assertStatus(Response::HTTP_OK);
+        $this->assertNotNull($user->fresh()->email_verified_at);
+    }
+
+    public function test_resend_verification_email()
+    {
+        // Create a user
+        $user = User::factory()->create(['email_verified_at' => null]);
+
+        // Simulate the authenticated user
+        $this->actingAs($user);
+
+        // Send a request to resend the verification email
+        $response = $this->post('api/email/resend');
+
+        // Assert the response
+        $response->assertStatus(Response::HTTP_OK);
     }
 }
